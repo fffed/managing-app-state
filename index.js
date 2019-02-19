300c423dc5aec51f36e49fd12cbde36b7b595c81
fffed <fffed@example.com>
2019-02-19 15:26:14 +0300

    add action creators

--- index.js
+++ index.js
@@ -45,6 +45,44 @@ const TOGGLE_TODO = 'TOGGLE_TODO'
 const ADD_GOAL = 'ADD_GOAL'
 const REMOVE_GOAL = 'REMOVE_GOAL'
 
+
+/*Action creators*/
+function addTodoAction (todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  }
+}
+
+function removeTodoAction (id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  }
+}
+
+function toggleTodoAction (id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  }
+}
+
+function addGoalAction (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoalAction (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+
 /* Create Reducers */
 function todos (state = [], action) {
   switch(action.type) {
@@ -92,60 +130,37 @@ const uns = store.subscribe(() => {
   console.log('The store changed.')
 })
 
-store.dispatch({
-  type: ADD_TODO,
-  todo: {
-    id: 0,
-    name: 'Walk the dog',
-    complete: false,
-  }
-})
 
-store.dispatch({
-  type: ADD_TODO,
-  todo: {
-    id: 1,
-    name: 'Wash the car',
-    complete: false,
-  }
-})
+store.dispatch(addTodoAction({
+  id: 0,
+  name: 'Walk the dog',
+  complete: false,
+}))
 
-store.dispatch({
-  type: ADD_TODO,
-  todo: {
-    id: 2,
-    name: 'Go to the gym',
-    complete: true,
-  }
-})
+store.dispatch(addTodoAction({
+  id: 1,
+  name: 'Wash the car',
+  complete: false,
+}))
 
-store.dispatch({
-  type: REMOVE_TODO,
-  id: 1
-})
+store.dispatch(addTodoAction({
+  id: 2,
+  name: 'Go to the gym',
+  complete: true,
+}))
 
-store.dispatch({
-  type: TOGGLE_TODO,
-  id: 0
-})
+store.dispatch(removeTodoAction(1))
 
-store.dispatch({
-  type: ADD_GOAL,
-  goal: {
-    id: 0,
-    name: 'Learn Redux'
-  }
-})
+store.dispatch(toggleTodoAction(0))
 
-store.dispatch({
-  type: ADD_GOAL,
-  goal: {
-    id: 1,
-    name: 'Lose 20 pounds'
-  }
-})
+store.dispatch(addGoalAction({
+  id: 0,
+  name: 'Learn Redux'
+}))
 
-store.dispatch({
-  type: REMOVE_GOAL,
-  id: 0
-})
+store.dispatch(addGoalAction({
+  id: 1,
+  name: 'Lose 20 pounds'
+}))
+
+store.dispatch(removeGoalAction(0))
