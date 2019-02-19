707f65feb8af203a1fca090a9c0b2e6314c22600
fffed <fffed@example.com>
2019-02-19 15:49:36 +0300

    add buttons with 'add actions'

--- index.js
+++ index.js
@@ -126,41 +126,31 @@ store.subscribe(() => {
   console.log('The new state is: ', store.getState())
 })
 
-const uns = store.subscribe(() => {
-  console.log('The store changed.')
-})
+function generateId () {
+  return Math.random().toString(36).substring(2) + (new Date()).getTime().toString(36);
+}
 
+function addTodo () {
+  const input = document.getElementById('todo')
+  const name = input.value
+  input.value = ''
+  store.dispatch(addTodoAction({
+    name,
+    complete: false,
+    id: generateId()
+  }))
+}
 
-store.dispatch(addTodoAction({
-  id: 0,
-  name: 'Walk the dog',
-  complete: false,
-}))
+function addGoal () {
+  const input = document.getElementById('goal')
+  const name = input.value
+  input.value = ''
+  store.dispatch(addGoalAction({
+    id: generateId(),
+    name,
+  }))
+}
 
-store.dispatch(addTodoAction({
-  id: 1,
-  name: 'Wash the car',
-  complete: false,
-}))
+document.getElementById('todoBtn').addEventListener('click', addTodo)
 
-store.dispatch(addTodoAction({
-  id: 2,
-  name: 'Go to the gym',
-  complete: true,
-}))
-
-store.dispatch(removeTodoAction(1))
-
-store.dispatch(toggleTodoAction(0))
-
-store.dispatch(addGoalAction({
-  id: 0,
-  name: 'Learn Redux'
-}))
-
-store.dispatch(addGoalAction({
-  id: 1,
-  name: 'Lose 20 pounds'
-}))
-
-store.dispatch(removeGoalAction(0))
+document.getElementById('goalBtn').addEventListener('click', addGoal)
